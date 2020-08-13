01ec8cab0cea8ae31affff338d22e61992dcdda6
Ankur Kumar <ank@leoank.me>
2020-08-14 00:47:50 +0530

    Add benchmarks

--- benchmarks/benchmark_main.cpp
+++ benchmarks/benchmark_main.cpp
@@ -1,3 +1,24 @@
 #include <benchmark/benchmark.h>
+#include <memory>
+#include <memtracker.hpp>
+#include <npas4/Npas4.h>
 
-BENCHMARK_MAIN();
+class TestMemoryManager : public benchmark::MemoryManager
+{
+  void Start() {}
+  void Stop(Result *result)
+  {
+    result->num_allocs = npas4::GetRAMSystemUsedByCurrentProcess() + npas4::GetRAMVirtualUsedByCurrentProcess();
+    result->max_bytes_used = npas4::GetRAMSystemUsedByCurrentProcess() + npas4::GetRAMVirtualUsedByCurrentProcess();
+  }
+};
+
+int main(int argc, char **argv)
+{
+  benchmark::Initialize(&argc, argv);
+  if (benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  std::unique_ptr<benchmark::MemoryManager> memory_manager(new TestMemoryManager());
+  benchmark::RegisterMemoryManager(memory_manager.get());
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::RegisterMemoryManager(nullptr);
+}
